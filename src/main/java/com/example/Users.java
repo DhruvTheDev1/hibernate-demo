8fb59e12b04ccefe1606ef6c1ef2d086fbf5d19a
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-05-20 15:20:13 +0100

    Pojo class

--- src/main/java/com/example/Users.java
+++ src/main/java/com/example/Users.java
@@ -1,5 +1,45 @@
 package com.example;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class Users {
+
+  @Id
+  @Column(name = "idusers")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "firstname")
+  private String firstName;
+
+  @Column(name = "lastname")
+  private String lastName;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
 }
