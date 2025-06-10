14dbbc21189446b7d9a31c56b06aebc64b494462
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-06-10 16:23:42 +0100

    Simple CRUD setup

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class App {
   public static void main(String[] args) {
     UserDAO userDAO = new UserDAO();
+    // basic example CRUD
+    System.out.println("Welcome!");
+    System.out.println("1 - Create User");
+    System.out.println("2 - List All Users");
+    System.out.println("3 - Get user by id");
+    System.out.println("4 - Get User by First Name");
+    System.out.println("5 - Update user's first name"); 
+    System.out.println("6 - Delete user by id");
     
     // create user
     // userDAO.createUser("Mariano", "Calderon");
